b4fd93cedda90b6b9bc5ef7e5d875d6ea96041cc
Mehtap Aygün <mehtap.aygun@icloud.com>
2023-11-16 04:00:18 +0300

    workshop7

---
+++ workshop_1411/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:workshop_1411/pages/main_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(useMaterial3: true), home: const MainPage()));
+}
---
+++ workshop_1411/lib/pages/main_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:workshop_1411/pages/expense_page.dart';
+import 'package:workshop_1411/widgets/new_expense.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        title: const Text("Expense App"),
+        backgroundColor: Colors.deepPurpleAccent,
+        actions: [
+          IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (ctx) {
+                    return NewExpense();
+                  });
+            },
+            icon: const Icon(Icons.add_box),
+            iconSize: 40,
+          )
+        ],
+      ),
+      body: const ExpensesPage(),
+    );
+  }
+}
